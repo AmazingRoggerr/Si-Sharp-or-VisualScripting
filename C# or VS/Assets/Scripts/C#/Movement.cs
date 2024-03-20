b93160761105c53ecae03c0049e4aa9bdf30affb
Ulugbek <amazingroggerr@gmail.com>
2024-03-21 00:26:50 +0500

    Unity 3-Dars Poekti

---
+++ C# or VS/Assets/Scripts/C#/Movement.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+namespace UzGameDev
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5;
+        [SerializeField] private float rotateSpeed = 600;
+        
+        private Rigidbody body;
+        private Animator animator;
+
+        private readonly int Velocity = Animator.StringToHash("Velocity");
+        
+        private Vector3 moveDirection;
+        
+        private void Awake()
+        {
+            body = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
+        }
+
+        //===============================================================
+        private void FixedUpdate()
+        {
+            var inputX = Input.GetAxis("Horizontal");
+            var inputY = Input.GetAxis("Vertical");
+            moveDirection = new Vector3(inputX * moveSpeed, body.velocity.y, inputY * moveSpeed);
+
+            body.velocity = moveDirection;
+        }
+        //===============================================================
+        private void Update()
+        {
+            var animVelocity = new Vector3(moveDirection.x, 0, moveDirection.z);
+            animator.SetFloat(Velocity, animVelocity.sqrMagnitude);
+            
+            if(moveDirection is {x:0, z:0 }) return;
+            var rotateDirection = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDirection, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
